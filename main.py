6cc1c0ec9a9b5c03e712d9f78b43ddc531d92d09
PacoPacorius <c.perdikis@yahoo.gr>
2021-10-02 19:21:56 +0300

    Plotting done 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 from wand.image import Image
 from wand.color import Color
 
@@ -7,6 +8,8 @@ from wand.color import Color
 
 # image -> Calculate perceived birghtness -> brightness number, phase (how will i get phase?)
 # Calculate perceived brightness: convert to grayscale (done), access pixel color value (done), add brightness value of each pixel(done), normalize(done), calc average of brightness(done)
+
+# Put the brightness values in an array
 
 # Draw plot:
 def calculate_brightness(filename):
@@ -36,8 +39,13 @@ def calculate_brightness(filename):
 
 
 cube=""
+bright_values = np.empty(60);
 
-for i in range(1, 81):
-    print("index: " + f"0{i:03}.png")
-    print(calculate_brightness(f"0{i:03}.png"))
+for i in range(1, 61):
+    print("index: " + f"0{i:03}.png");
+    bright_values[i - 1] = calculate_brightness(f"0{i:03}.png");
+    print(bright_values[i - 1]);
 
+plt.figure();
+plt.plot(bright_values[0:61]);
+plt.show();
