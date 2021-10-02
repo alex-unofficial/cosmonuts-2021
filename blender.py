8084b8d244cc6834824d0cc85ebcc2e004689c1e
weird-alex <athanasiadisalex2@gmail.com>
2021-10-02 23:54:03 +0300

    edited blender.py

--- blender.py
+++ blender.py
@@ -1,10 +1,15 @@
 import bpy
-from mathutils import Euler
+from cmath import pi
+from mathutils import Vector, Euler
 
-model = bpy.data.objects['model']
+model = bpy.data.objects['216kleopatra']
 
-for i in range(1, 31):
-    model.delta_rotation_euler = Euler((0.0, 0.0, (i-1)*12.0), 'XYZ')
+render_dir = "/home/alex/Desktop/stuff/blender/renders/"
 
-    bpy.context.scene.render.filepath = f"blender/renders/{i:03}.png"
+N = 60
+
+for i in range(1, N + 1):
+    model.delta_rotation_euler = Euler((0.0, 0.0, (i-1)*2*pi/N), 'XYZ')
+
+    bpy.context.scene.render.filepath = render_dir +  f"{i:03}.png"
     bpy.ops.render.render(write_still=True)
